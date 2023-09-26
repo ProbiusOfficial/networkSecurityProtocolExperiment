f9593d0c9abba3c205ee8b893e4178807f59f87f
探姬 <41804496+ProbiusOfficial@users.noreply.github.com>
2023-09-26 16:25:40 +0800

    update

--- Ex_1/main.py
+++ Ex_1/main.py
@@ -1,16 +1,20 @@
 import winreg as reg
 import ctypes
+import time
+
 
 def modify_registry():
 
-    # 初始化值项dword的值为None。
+    # 初始化值
+    regKey = reg.HKEY_CURRENT_USER
+    regPath = r"Software\Microsoft\Windows\CurrentVersion\Policies\Test"
     dword_value = None
 
     # 用RegOpenKeyEx() 函数打开注册表项:
     try:
         key = reg.OpenKeyEx(
-            reg.HKEY_CURRENT_USER,
-            r"Software\Microsoft\Windows\CurrentVersion\Policies\Test",
+            regKey,
+            regPath,
             0,
             reg.KEY_ALL_ACCESS
         )
@@ -19,8 +23,8 @@ def modify_registry():
     except FileNotFoundError:
         # 如果该注册表项不存在，则用RegCreateKeyEx() 函数创建该项。
         key = reg.CreateKeyEx(
-            reg.HKEY_CURRENT_USER,
-            r"Software\Microsoft\Windows\CurrentVersion\Policies\Test",
+            regKey,
+            regPath,
             0,
             reg.KEY_ALL_ACCESS
         )
@@ -29,8 +33,20 @@ def modify_registry():
 
     # 如果该项为新创建的，则新建立一个REG_SZ值项DisableChangePassword和一个REG_DWORD值项dword。
     if new_key:
-        reg.SetValueEx(key, "DisableChangePassword", 0, reg.REG_SZ, "")
-        reg.SetValueEx(key, "dword", 0, reg.REG_DWORD, 0)
+        reg.SetValueEx(
+            key, 
+            "DisableChangePassword", 
+            0, 
+            reg.REG_SZ, 
+            ""
+        )
+        reg.SetValueEx(
+            key, 
+            "dword", 
+            0, 
+            reg.REG_DWORD, 
+            0
+        )
     else:
         # 如果该值项已经存在，则用RegQueryValueEx() 读取值项DisableRegistryTools的dword的值。
         try:
@@ -48,8 +64,32 @@ def modify_registry():
     # 用RegFlushKey() 函数将对注册表项的修改写入注册表。
     reg.FlushKey(key)
 
+    # 还原恢复注册表
+    def recover():
+        
+        for i in range(10, 0, -1):
+            print(f"还原将在{i}秒后进行,请及时记录注册表实验数据")
+            time.sleep(1)
+        if new_key:
+            reg.DeleteKeyEx(
+                regKey,
+                regPath,
+                reg.KEY_ALL_ACCESS,
+                0
+            )
+        else:
+            reg.SetValueEx(
+            key, 
+            "dword", 
+            0, 
+            reg.REG_DWORD, 
+            dword_value
+        )
+
+    recover()
+
     # 用RegCloseKey() 函数关闭打开的注册表项。
     reg.CloseKey(key)
-
+    
 # 执行函数
 modify_registry()
